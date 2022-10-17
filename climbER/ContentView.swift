b046eeda47b9db1e92297df09c1cccfecbd25b97
杨阳 <yangyang@xiaoyun.com>
2022-10-17 22:11:18 +0800

    Initial Commit

---
+++ climbER/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  climbER
+//
+//  Created by 杨阳 on 2022/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ climbER/climbERApp.swift
@@ -0,0 +1,17 @@
+//
+//  climbERApp.swift
+//  climbER
+//
+//  Created by 杨阳 on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct climbERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
